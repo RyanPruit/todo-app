6c54adc6391a4f6754deff95f51c591ba6a1a7dc
sonialwani <alwani.soni@gmail.com>
2023-08-28 12:07:47 +0530

    created web.py for to-do app

--- web.py
+++ web.py
@@ -8,11 +8,26 @@ def add_todo():
     functions.todo.append(x)
     functions.write_file()
 
-for do in functions.todo:
-    st.checkbox(do)
 
 
-st.text_input(label="",placeholder="Add new todo...",on_change=add_todo,key='new_todo')
+def delete_todo():
+    for index, do in enumerate(functions.todo):
+        y = st.session_state[index]
+
+        functions.todo.remove(y)
+        functions.write_file()
+
+for index, do in enumerate(functions.todo):
+
+    checkbox = st.checkbox(do,key=index)
+
+    if checkbox:
+        functions.todo.remove(do)
+        functions.write_file()
+
+
+
+st.text_input(label="",placeholder="Add new todo...",on_change=add_todo,key='new_todo')+"\n"
 
 
 
